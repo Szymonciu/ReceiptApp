b132af908011bf75e8bc1990d9ead86d5600af3b
Szymonciu <37048857+Szymonciu@users.noreply.github.com>
2019-02-20 20:36:54 +0100

    Setting receipt details

--- app/src/main/java/com/example/pc/receiptapp/features/details/ReceiptDetailsActivity.java
+++ app/src/main/java/com/example/pc/receiptapp/features/details/ReceiptDetailsActivity.java
@@ -1,8 +1,10 @@
 package com.example.pc.receiptapp.features.details;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.pc.receiptapp.R;
 import com.example.pc.receiptapp.core.Receipt;
@@ -11,15 +13,31 @@ public class ReceiptDetailsActivity extends AppCompatActivity {
 
     public static final String RECEIPT_KEY = "receipt key";
 
+    private TextView title;
+    private TextView place;
+    private TextView date;
+    private ImageView photo;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipt_details);
-        getReceiptFromExtras();
+        Receipt receipt = (Receipt) getIntent().getSerializableExtra(RECEIPT_KEY);
+        initViews();
+        title.setText(receipt.getTitle());
+        place.setText(receipt.getPlace());
+        date.setText(receipt.getDate());
+        photo.setImageURI(Uri.parse(receipt.getImagePath()));
+
     }
 
-    private void getReceiptFromExtras() {
-        Receipt receipt = (Receipt) getIntent().getSerializableExtra(RECEIPT_KEY);
-        Toast.makeText(this, receipt.toString(), Toast.LENGTH_SHORT).show();
+    private void initViews() {
+        title = findViewById(R.id.title_value);
+        place = findViewById(R.id.place_value);
+        date = findViewById(R.id.date_value);
+        photo = findViewById(R.id.image);
     }
+
+
 }
